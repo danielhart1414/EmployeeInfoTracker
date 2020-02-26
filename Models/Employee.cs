1a5804f05f31c6e577b8b2a6f1cb19aeaf411795
danielhart1414 <daniel_hart@live.com>
2020-02-25 16:39:18 -0800

    Initial commit

---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmployeeInfoTracker
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Department { get; set; }
+
+        public double Salary { get; set; }
+    }
+}
