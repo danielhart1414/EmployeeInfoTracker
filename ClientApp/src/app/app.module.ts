5ab4d5da84aeab3fa7271a813dc3c6d6bd2a14d6
danielhart1414 <daniel_hart@live.com>
2020-02-27 17:01:50 -0800

    Added form, table, and HTTP get to tracker

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -19,7 +19,7 @@ import { TrackerComponent } from './tracker/tracker.component';
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
-    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'tracker', component: TrackerComponent },
--- ClientApp/src/app/tracker/tracker.component.ts
+++ ClientApp/src/app/tracker/tracker.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tracker',
@@ -6,10 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tracker.component.css']
 })
 export class TrackerComponent implements OnInit {
+  employeeForm;
+  public employees: Employee[];
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    http: HttpClient,
+    @Inject('BASE_URL') baseUrl: string
+  ) {
+    this.employeeForm = this.formBuilder.group({
+      id: '',
+      firstName: '',
+      lastName: '',
+      department: '',
+      salary: ''
+    });
+
+    http.get<Employee[]>(baseUrl + 'employee')
+      .subscribe(
+        result => { this.employees = result; },
+        error => console.error(error)
+      );
+  }
 
   ngOnInit() {
   }
 
+  onSubmit(employeeData) {
+    // Process employee data here
+    this.employeeForm.reset();
+
+    window.alert('Your employee data has been submitted');
+  }
+}
+
+interface Employee {
+  id: string;
+  firstName: string;
+  lastName: string;
+  department: string;
+  salary: number;
 }
